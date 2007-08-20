020f4064c6fc7e361ef84cd24cf64956276e61fb
Doug McCorkle <doug.mccorkle@gmail.com>
2007-08-20 20:29:06 +0000

    fixed osgPT build. Exported ConfigFileReader so that osgPT could use it in the plugin. made the osgPT build dependent on OSGOQ_INC_DIR and OSGOQ_LIB_DIR.

---
+++ build.bat
@@ -0,0 +1,13 @@
+set PATH=%PATH%;C:\devEnv\VES.1.0-Deps\OSG_svn\install\bin
+set PATH=%PATH%;C:\devEnv\VES.1.0-Deps\OSG_svn\OpenThreads\bin\win32
+set PATH=%PATH%;
+set OSG_FILE_PATH=C:\devEnv\VES.1.0-Deps\OSG_svn\osgOQ\data
+set OSG_FILE_PATH=%OSG_FILE_PATH%;C:\devEnv\VES.1.0-Deps\OSG_OP_OT-1.2\OpenSceneGraph-Data
+set OSG_POLYTRANS_CONFIG_FILE=D:\devEnv\VES-Deps_1.1\source\skewmatrix\osgPT\trunk\data\ptconfigfile.txt
+set OSGOQ_CONFIG_FILE=D:\devEnv\VES-Deps_1.1\source\skewmatrix\osgOQ\trunk\data\osgOQConfigFile.txt
+set OSG_INC_DIR=D:\devEnv\VES-Deps_1.1\prebuiltInstalls\OSG_1.2_Pre-Compile_vc8.0\include
+set OSG_LIB_DIR=D:\devEnv\VES-Deps_1.1\prebuiltInstalls\OSG_1.2_Pre-Compile_vc8.0\lib\win32
+set OSGOQ_INC_DIR=D:\devEnv\VE-Suite_1.1\external\osgOQ\include
+set OSGOQ_LIB_DIR=D:\devEnv\VE-Suite_1.1\lib\win32
+"C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" D:\devEnv\VES-Deps_1.1\source\skewmatrix\osgPT\trunk\VS7\PolyTransDev.sln
+pause
