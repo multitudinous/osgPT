c6e5c8981f42a21b8389893ec9578215f31e38c7
Paul Martz <pmartz@skew-matrix.com>
2011-03-02 21:35:16 +0000

    Stub for osgText support.

---
+++ OSGExport/osgText.cpp
@@ -0,0 +1,11 @@
+// Copyright 2011 Skew Matrix Software LLC. All rights reserved.
+
+#include "main.h"
+
+
+
+Nd_Bool
+osgProcessText( Nd_Walk_Tree_Info *Nv_Info, char *master_object, osg::Geode* geode )
+{
+    return( Ni_TRUE );
+}
